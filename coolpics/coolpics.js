820a6d67a31c12b7b295af609413cb5858f7a7e3
davidawells <wel20012@byui.edu>
2025-05-18 00:05:37 -0600

    pass fo real

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,14 +1,18 @@
-const image = document.getElementById("image");
+// const image = document.getElementById("image");
+// apply clickability to all images
+const images = document.querySelectorAll(".images");
 const dialogbox = document.getElementById("dialogbox");
-image.onclick = function() {
-    if (dialogbox.style.display === "flex") {
-        dialogbox.style.display = "none";
-    } else {
+images.forEach((image) => {
+    image.onclick = function() {
         dialogbox.style.display = "flex";
     }
-}
-// document.onclick = function(event) {
-//     if (dialogbox.style.display === "flex") {
-//         dialogbox.style.display = "none";
-//     }
+}); 
+
+// image.onclick = function() {
+//     dialogbox.style.display = "flex";
 // }
+dialogbox.onclick = function(event) {
+    if (dialogbox.style.display === "flex") {
+        dialogbox.style.display = "none";
+    }
+}
