b938a579910bc2fe9e2a1f8722e5753d3f96671d
davidawells <wel20012@byui.edu>
2025-05-18 00:20:09 -0600

    works local but still not on github pages

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,18 +1,14 @@
-// const image = document.getElementById("image");
 // apply clickability to all images
 const images = document.querySelectorAll(".images");
 const dialogbox = document.getElementById("dialogbox");
+const dialogImage = dialogbox.querySelector("img");
 images.forEach((image) => {
     image.onclick = function() {
         dialogbox.style.display = "flex";
     }
 }); 
-
-// image.onclick = function() {
-//     dialogbox.style.display = "flex";
-// }
 dialogbox.onclick = function(event) {
-    if (dialogbox.style.display === "flex") {
+    if (event.target === dialogbox) {
         dialogbox.style.display = "none";
     }
 }
